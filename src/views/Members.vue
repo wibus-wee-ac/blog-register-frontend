875b22a7841465e0de6a586840f051c6f0c8aaa8
jsun969 <i@jsun969.cn>
2021-07-01 11:14:38 +0800

    :sparkles: Add link to each member's blog url

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -2,7 +2,11 @@
   <el-table :data="memberData" border lazy>
     <el-table-column prop="name" label="昵称"> </el-table-column>
     <el-table-column prop="blog_name" label="博客名"> </el-table-column>
-    <el-table-column prop="blog_url" label="博客地址"> </el-table-column>
+    <el-table-column prop="blog_url" label="博客地址">
+      <template #default="scope">
+        <el-link :href="scope.row.blog_url" type="primary" target="_blank">{{ scope.row.blog_url }}</el-link>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
@@ -10,10 +14,18 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import { getPassedUsers } from '../api';
 
+interface passedUserData {
+  name: string;
+  blog_name: string;
+  blog_url: string;
+}
+
+type passedUserDatas = passedUserData[];
+
 export default defineComponent({
   name: 'Members',
   setup() {
-    const memberData = ref([]);
+    const memberData = ref<passedUserDatas>([]);
     onMounted(async () => {
       const { data } = await getPassedUsers();
       memberData.value = data;
